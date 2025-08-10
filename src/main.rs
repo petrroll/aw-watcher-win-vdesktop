42739979d14f9918a7f66b3bbbbf9632a4a525cd
Petr Houska <houskape@gmail.com>
2025-08-10 18:45:29 +0200

    Implement periodic current vdesktop watcher

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use aw_client_rust::AwClient;
 use aw_models::{Bucket, Event};
 use chrono::TimeDelta;
 use serde_json::{Map, Value};
+use tokio::signal;
 
 async fn create_bucket(
     aw_client: &AwClient,
@@ -27,31 +28,59 @@ async fn create_bucket(
     Ok(res)
 }
 
+fn get_current_vdesktop() -> String {
+    let vdesktop = winvd::get_current_desktop().unwrap();
+    print!("{:?}", vdesktop);
+
+    let vdesktop_name = vdesktop.get_name().unwrap();
+    if vdesktop_name.is_empty() {
+        let vdesktop_id = vdesktop.get_index().unwrap();
+        return format!("Desktop {}", vdesktop_id + 1);
+    }
+
+    vdesktop_name
+}
 
 #[tokio::main]
 async fn main() {
     let port = 5666; // the testing port 
     let aw_client = AwClient::new("localhost", port, "aw-watcher-win-vdestkop").unwrap();
     let bucket_id = format!("aw-watcher-win-vdesktop_{}", aw_client.hostname);
-
+ 
     println!("Creating bucket: {}", bucket_id);
-
     create_bucket(&aw_client, bucket_id.clone()).await.unwrap();
 
-    let mut vdesktop_data = Map::new();
-    vdesktop_data.insert(
-        "vdesktop".to_string(),
-        Value::String("Desktop 1".to_string()),
-    );
+    let ctrl_c = signal::ctrl_c();
+    tokio::pin!(ctrl_c);
 
-    let now = chrono::Utc::now();
-    let shutdown_event = Event {
-        id: None,
-        timestamp: now,
-        duration: TimeDelta::seconds(420),
-        data: vdesktop_data,
-    };
-    aw_client.insert_event(&bucket_id, &shutdown_event).await.unwrap();
+    loop {
+        let mut vdesktop_data = Map::new();
+        let curr_vdesktop_name = get_current_vdesktop();
+        vdesktop_data.insert(
+            "vdesktop".to_string(),
+            Value::String(curr_vdesktop_name),
+        );
+
+        let now = chrono::Utc::now();
+        let shutdown_event = Event {
+            id: None,
+            timestamp: now,
+            duration: TimeDelta::seconds(12),
+            data: vdesktop_data,
+        };
+
+        aw_client.heartbeat(&bucket_id, &shutdown_event, 10.0).await.unwrap();
+
+        tokio::select! {
+            _ = &mut ctrl_c => {
+                println!("Ctrl+C received, exiting...");
+                break;
+            }
+            _ = tokio::time::sleep(tokio::time::Duration::from_secs(8)) => {
+                // Your periodic code here
+            }
+        }
+    }
 
     let events = aw_client.get_events(&bucket_id, None, None, Some(1)).await.unwrap();
     print!("{:?}", events); // prints a single event
